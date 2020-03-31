1ceb67591a10074afee4cc4dfd34d3636dca8cce
Gustavo Bicalho <gusbicalho@gmail.com>
2020-03-31 20:04:53 -0300

    add type signatures

--- hslox/test/HSLox/ASTPrinterSpec.hs
+++ hslox/test/HSLox/ASTPrinterSpec.hs
@@ -7,6 +7,7 @@ import HSLox.Token (Token (..))
 import qualified HSLox.Token as Token
 import Test.Hspec
 
+spec :: Spec
 spec = do
   describe "printAST" $ do
     it "should print the example from the book correctly" $ do
@@ -14,8 +15,10 @@ spec = do
     it "should print a more complex expression correctly" $ do
       printAST biggerExpr `shouldBe` printedBiggerExpr
 
+printedBookExpr :: T.Text
 printedBookExpr = "(* (- 123.0) (group 45.67))"
 
+bookExpr :: Expr
 bookExpr =
   binary
     (unary (Token "-" Token.MINUS Nothing 1) (numValue 123))
@@ -23,8 +26,10 @@ bookExpr =
     (grouping
       (numValue 45.67))
 
+printedBiggerExpr :: T.Text
 printedBiggerExpr = "(+ (group (- 3.0 (- 1.0))) \"asd\")"
 
+biggerExpr :: Expr
 biggerExpr =
   binary
     (grouping
