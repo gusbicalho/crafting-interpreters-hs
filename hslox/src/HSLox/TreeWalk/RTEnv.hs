21db435fbd1c4d447cb843916643451c18ce857a
Gustavo Bicalho <gusbicalho@gmail.com>
2020-04-10 12:07:26 -0300

    add enclosing environments

--- hslox/src/HSLox/TreeWalk/RTEnv.hs
+++ hslox/src/HSLox/TreeWalk/RTEnv.hs
@@ -2,6 +2,7 @@ module HSLox.TreeWalk.RTEnv where
 
 import Control.Effect.Throw
 import Control.Effect.State
+import Control.Applicative
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
@@ -12,19 +13,26 @@ import HSLox.TreeWalk.RTValue (RTValue (..))
 
 type BindingName = T.Text
 
-data RTEnv = RTEnv { rtEnvBindings :: Map BindingName RTValue }
+data RTEnv = RTEnv { rtEnvBindings :: Map BindingName RTValue
+                   , rtEnvEnclosing :: Maybe RTEnv
+                   }
 
 newEnv :: RTEnv
-newEnv = RTEnv Map.empty
+newEnv = RTEnv Map.empty Nothing
 
-define :: BindingName -> RTValue -> RTEnv -> RTEnv
-define name val env = RTEnv $ Map.insert name val (rtEnvBindings env)
+overBindings :: (Map BindingName RTValue -> Map BindingName RTValue) -> RTEnv -> RTEnv
+overBindings f env = env { rtEnvBindings = f (rtEnvBindings env) }
+
+bindNameToValue :: BindingName -> RTValue -> RTEnv -> RTEnv
+bindNameToValue name val = overBindings $ Map.insert name val
 
 defineM :: Has (State RTEnv) sig m => BindingName -> RTValue -> m ()
-defineM name val = modify $ define name val
+defineM name val = modify $ bindNameToValue name val
 
 getBoundValue :: BindingName -> RTEnv -> Maybe RTValue
-getBoundValue name env = Map.lookup name (rtEnvBindings env)
+getBoundValue name env =
+  Map.lookup name (rtEnvBindings env)
+  <|> (getBoundValue name =<< (rtEnvEnclosing env))
 
 getBoundValueM :: Has (State RTEnv) sig m
                => Has (Throw RTError) sig m
@@ -36,12 +44,24 @@ getBoundValueM tk = do
     Just val -> pure val
     Nothing -> RTError.throwRT tk $ "Undefined variable '" <> name <> "'."
 
+assign :: Token -> RTValue -> RTEnv -> Maybe RTEnv
+assign tk val env = do
+  let name = tokenLexeme tk
+  let bindings = rtEnvBindings env
+  case Map.lookup name bindings of
+    Just _ -> Just $ bindNameToValue name val env
+    Nothing -> do
+      parent <- rtEnvEnclosing env
+      parent <- assign tk val parent
+      pure env { rtEnvEnclosing = Just parent }
+
 assignM :: Has (State RTEnv) sig m
         => Has (Throw RTError) sig m
         => Token -> RTValue -> m ()
 assignM tk val = do
-  bindings <- gets rtEnvBindings
-  let name = tokenLexeme tk
-  case Map.lookup name bindings of
-    Just _ -> modify $ define name val
-    Nothing -> RTError.throwRT tk $ "Undefined variable '" <> name <> "'."
+  env <- get
+  case assign tk val env of
+    Just env -> put env
+    Nothing -> RTError.throwRT tk $ "Undefined variable '"
+                                 <> tokenLexeme tk
+                                 <> "'."
