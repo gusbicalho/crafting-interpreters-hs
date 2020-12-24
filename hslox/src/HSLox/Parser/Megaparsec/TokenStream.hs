53164420578ea4697a8b77b6ecbb35127e9d8cda
Gustavo Bicalho <gusbicalho@gmail.com>
2020-12-24 04:36:20 +0000

    remove stack/hpack

--- hslox/src/HSLox/Parser/Megaparsec/TokenStream.hs
+++ hslox/src/HSLox/Parser/Megaparsec/TokenStream.hs
@@ -1,6 +1,5 @@
 module HSLox.Parser.Megaparsec.TokenStream where
 
-
 import Data.Foldable
 import Data.Sequence (Seq (..))
 import Data.List.NonEmpty (NonEmpty (..))
@@ -42,34 +41,34 @@ instance Stream.Stream TokenStream where
     let (front, back) = Seq.spanl pred seq
     in (front, TokenStream back)
 
-  showTokens _ (tk :| []) = T.unpack (tokenLexeme tk)
-  showTokens _ (tk :| tks)
-    = T.unpack $ tokenLexeme tk
-              <> snd (foldl' appendTk (tokenLine tk, "") tks)
-    where
-      appendTk (prevLine, s) tk
-        | prevLine == tokenLine tk = (tokenLine tk, s <> " "  <> tokenLexeme tk)
-        | otherwise                = (tokenLine tk, s <> "\n" <> tokenLexeme tk)
+  -- showTokens _ (tk :| []) = T.unpack (tokenLexeme tk)
+  -- showTokens _ (tk :| tks)
+  --   = T.unpack $ tokenLexeme tk
+  --             <> snd (foldl' appendTk (tokenLine tk, "") tks)
+  --   where
+  --     appendTk (prevLine, s) tk
+  --       | prevLine == tokenLine tk = (tokenLine tk, s <> " "  <> tokenLexeme tk)
+  --       | otherwise                = (tokenLine tk, s <> "\n" <> tokenLexeme tk)
 
-  tokensLength p tks = length (Stream.showTokens p tks)
+  -- tokensLength p tks = length (Stream.showTokens p tks)
 
-  reachOffset offset posSt@PosState { pstateInput
-                                    , pstateOffset
-                                    , pstateSourcePos
-                                    }
-    = ( lineText
-      , posSt { pstateInput = TokenStream back
-              , pstateOffset = offset
-              , pstateSourcePos =
-                pstateSourcePos { sourceLine = finalLineNum
-                                , sourceColumn = mkPos 1
-                                }
-              })
-    where
-      (front, back) = Seq.splitAt (offset - pstateOffset) . unTokenStream $ pstateInput
-      finalLineNum = case front of
-                      _ :|> tk -> mkPos $ tokenLine tk
-                      _ -> case back of
-                        tk :<| _ -> mkPos $ tokenLine tk
-                        _ -> sourceLine pstateSourcePos
-      lineText = "<empty line>"
+  -- reachOffset offset posSt@PosState { pstateInput
+  --                                   , pstateOffset
+  --                                   , pstateSourcePos
+  --                                   }
+  --   = ( lineText
+  --     , posSt { pstateInput = TokenStream back
+  --             , pstateOffset = offset
+  --             , pstateSourcePos =
+  --               pstateSourcePos { sourceLine = finalLineNum
+  --                               , sourceColumn = mkPos 1
+  --                               }
+  --             })
+  --   where
+  --     (front, back) = Seq.splitAt (offset - pstateOffset) . unTokenStream $ pstateInput
+  --     finalLineNum = case front of
+  --                     _ :|> tk -> mkPos $ tokenLine tk
+  --                     _ -> case back of
+  --                       tk :<| _ -> mkPos $ tokenLine tk
+  --                       _ -> sourceLine pstateSourcePos
+  --     lineText = "<empty line>"
