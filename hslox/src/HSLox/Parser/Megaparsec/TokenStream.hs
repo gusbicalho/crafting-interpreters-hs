64dee5e49fbc6cee68f7c201324a075ebe68ddd4
Gustavo Bicalho <gusbicalho@gmail.com>
2020-12-24 04:38:23 +0000

    remove commented code

--- hslox/src/HSLox/Parser/Megaparsec/TokenStream.hs
+++ hslox/src/HSLox/Parser/Megaparsec/TokenStream.hs
@@ -2,11 +2,8 @@ module HSLox.Parser.Megaparsec.TokenStream where
 
 import Data.Foldable
 import Data.Sequence (Seq (..))
-import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.Sequence as Seq
-import qualified Data.Text as T
 import HSLox.Token (Token (..))
-import Text.Megaparsec (PosState (..), SourcePos (..), mkPos)
 import qualified Text.Megaparsec.Stream as Stream
 
 newtype TokenStream = TokenStream { unTokenStream :: Seq Token }
@@ -40,35 +37,3 @@ instance Stream.Stream TokenStream where
   takeWhile_ pred (TokenStream seq) =
     let (front, back) = Seq.spanl pred seq
     in (front, TokenStream back)
-
-  -- showTokens _ (tk :| []) = T.unpack (tokenLexeme tk)
-  -- showTokens _ (tk :| tks)
-  --   = T.unpack $ tokenLexeme tk
-  --             <> snd (foldl' appendTk (tokenLine tk, "") tks)
-  --   where
-  --     appendTk (prevLine, s) tk
-  --       | prevLine == tokenLine tk = (tokenLine tk, s <> " "  <> tokenLexeme tk)
-  --       | otherwise                = (tokenLine tk, s <> "\n" <> tokenLexeme tk)
-
-  -- tokensLength p tks = length (Stream.showTokens p tks)
-
-  -- reachOffset offset posSt@PosState { pstateInput
-  --                                   , pstateOffset
-  --                                   , pstateSourcePos
-  --                                   }
-  --   = ( lineText
-  --     , posSt { pstateInput = TokenStream back
-  --             , pstateOffset = offset
-  --             , pstateSourcePos =
-  --               pstateSourcePos { sourceLine = finalLineNum
-  --                               , sourceColumn = mkPos 1
-  --                               }
-  --             })
-  --   where
-  --     (front, back) = Seq.splitAt (offset - pstateOffset) . unTokenStream $ pstateInput
-  --     finalLineNum = case front of
-  --                     _ :|> tk -> mkPos $ tokenLine tk
-  --                     _ -> case back of
-  --                       tk :<| _ -> mkPos $ tokenLine tk
-  --                       _ -> sourceLine pstateSourcePos
-  --     lineText = "<empty line>"
